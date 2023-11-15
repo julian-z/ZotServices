c74230f5f3ffaa24778037e8dbf4ff91528cfeba
Julian Zulfikar <jzulfika@uci.edu>
2023-11-15 10:56:09 -0800

    Added ratings to search page and fixed scrollbars

--- client/src/components/ServicePreview/ServicePreview.js
+++ client/src/components/ServicePreview/ServicePreview.js
@@ -9,7 +9,37 @@ function ServicePreview(props) {
     >
       <img src={props.image} alt="Service Preview"></img>
 
-      <p style={{ fontWeight: "500", margin: "0 0 0 1vw" }}>{props.price}</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "80%",
+          margin: "0",
+        }}
+      >
+        <p
+          style={{
+            fontWeight: "500",
+            margin: "0 0 0 1vw",
+            fontSize: "0.75rem",
+          }}
+          class="service-preview-rating"
+        >
+          {props.price}
+        </p>
+        <p
+          style={{
+            fontWeight: "500",
+            margin: "0 0 0 1vw",
+            fontSize: "0.75rem",
+          }}
+          class="service-preview-rating"
+        >
+          ⭐ {props.rating}
+        </p>
+      </div>
       <p style={{ fontWeight: "400", margin: "0 0 0 1vw" }}>{props.title}</p>
       <p style={{ fontWeight: "300", fontSize: "0.8rem", margin: "0 0 0 1vw" }}>
         {props.location}
--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -9,72 +9,72 @@ function SearchPage() {
       sid: 1,
       uid: 1,
       image:
-        "https://cdn.discordapp.com/attachments/1174181819793539182/1174181828916154439/image.png?ex=6566a90b&is=6554340b&hm=41254e7c8ff7c8d1b1929f23579ccbb639806556907af62439d66aae82a7bec5&",
+        "https://cdn.discordapp.com/attachments/1174181819793539182/1174411306405736559/image.png?ex=65677ec2&is=655509c2&hm=80aab0c3550427c744e75c27fb7dfa710ddfffd517b3e48760a651a912711fdc&",
       title: "ZotCutz",
       category: "Beauty",
       location: "Stanford Court",
-      price: "$15 - $20",
-      rating: "5.0",
+      price: "$15",
+      rating: 5.0,
     },
 
     {
       sid: 2,
       uid: 2,
       image:
-        "https://cdn.discordapp.com/attachments/1174181819793539182/1174191758008127568/image.png?ex=6566b24a&is=65543d4a&hm=2880fa4111e6c77ca779abdebad4fc202860b4555215782576105944b1910e9e&",
+        "https://cdn.discordapp.com/attachments/1174181819793539182/1174411181625180182/image.png?ex=65677ea5&is=655509a5&hm=40f1f7ba08b47a1b74231bfde9a55987abab3d0114a2be8dc13b31af254d22e0&",
       title: "Custom Petr Stickers",
       category: "Arts",
       location: "Middle Earth",
       price: "$2",
-      rating: "4.8",
+      rating: 4.8,
     },
 
     {
       sid: 3,
       uid: 3,
       image:
-        "https://cdn.discordapp.com/attachments/1174181819793539182/1174191805382803537/2020-01-14.png?ex=6566b255&is=65543d55&hm=62b89f18852446f865459fea7bcc71faab846cb0e5942a5cfc623b3eeadd9c36&",
-      title: "Nails",
+        "https://cdn.discordapp.com/attachments/1174181819793539182/1174410887340228638/image.png?ex=65677e5f&is=6555095f&hm=76fa68c43b1b9e2490f7eb03671d946f634adf2522bff481d78c867e3887e2c6&",
+      title: "AntNails",
       category: "Beauty",
       location: "Mesa Court",
-      price: "$20 - $30",
-      rating: "4.3",
+      price: "$20",
+      rating: 4.3,
     },
 
     {
       sid: 4,
       uid: 4,
       image:
-        "https://cdn.discordapp.com/attachments/1174181819793539182/1174208557630115880/image.png?ex=6566c1ef&is=65544cef&hm=b97010b838322440be75c085b8f649bb645371a1278cdc2561a5db8ebdc838c5&",
+        "https://cdn.discordapp.com/attachments/1174181819793539182/1174410610306461737/image.png?ex=65677e1c&is=6555091c&hm=c488d8cc30ab938741f8cb63f841ec5f584211674c51306b1ab3a43686fea4ff&",
       title: "Floor Cleaning",
       category: "Cleaning",
       location: "Vista Del Campo Norte",
       price: "$10",
-      rating: "4.5",
+      rating: 4.5,
     },
 
     {
       sid: 5,
       uid: 5,
       image:
-        "https://cdn.discordapp.com/attachments/1174181819793539182/1174209918291681330/image.png?ex=6566c334&is=65544e34&hm=0e4cd2b190ea180b96fc05656b58df7ebf44fbcee67d020c15a2f34077d0c14f&",
+        "https://cdn.discordapp.com/attachments/1174181819793539182/1174410265211715774/image.png?ex=65677dca&is=655508ca&hm=9cd753e4dcc78c51b83abe1757c3ea6c50457e6cf14ff28ca09fa536c75f197e&",
       title: "Discounted DoorDash",
       category: "Delivery",
       location: "Irvine, CA",
       price: "$10",
-      rating: "4.3",
+      rating: 4.3,
     },
 
     {
       sid: 6,
       uid: 6,
       image:
-        "https://cdn.discordapp.com/attachments/1174181819793539182/1174209356175253544/image.png?ex=6566c2ae&is=65544dae&hm=f55dc527d4da539e229e3bb867ef4ebef9cce214c4b1ca4e8cd3faf1e97f051c&",
+        "https://cdn.discordapp.com/attachments/1174181819793539182/1174410418819694684/image.png?ex=65677def&is=655508ef&hm=df63b290f49666e9cdaf29f1984299d79ce82caf90c8a8de0dc939253d4762ee&",
       title: "Grocery Delivery",
       category: "Delivery",
       location: "Irvine, CA",
       price: "$5",
-      rating: "4.9",
+      rating: 4.9,
     },
   ];
 
@@ -82,7 +82,12 @@ function SearchPage() {
     <div className="row">
       <NavBar></NavBar>
 
-      <div className="col-9" style={{ padding: "2vh 2vw" }}>
+      <div
+        className="col-9 services-grid"
+        style={{
+          padding: "2vh 2vw",
+        }}
+      >
         <h2>All Services</h2>
 
         <div
@@ -111,11 +116,26 @@ function SearchPage() {
         </div>
       </div>
 
-      <div className="col-3 sidebar" style={{ padding: "2vh 2vw" }}>
+      <div
+        className="col-3 sidebar"
+        style={{
+          padding: "2vh 2vw",
+          overflowY: "auto",
+          maxHeight: "90vh",
+          position: "fixed",
+          top: "10vh",
+          right: 0,
+          height: "500vh",
+        }}
+      >
         <h4>Browse 🔎</h4>
 
         <form>
-          <input placeholder="Search by title..." id="sidebar-search"></input>
+          <input
+            placeholder="Search by title..."
+            autoComplete="off"
+            id="sidebar-search"
+          ></input>
         </form>
 
         <hr></hr>
@@ -128,8 +148,8 @@ function SearchPage() {
           }}
         >
           <h5>Sort By</h5>
-          <button className="sidebar-category">Price</button>
-          <button className="sidebar-category">Ratings</button>
+          <button className="sidebar-category">Low Price</button>
+          <button className="sidebar-category">High Ratings</button>
         </div>
 
         <hr></hr>
@@ -142,7 +162,7 @@ function SearchPage() {
           }}
         >
           <h5>Categories</h5>
-          <button className="sidebar-category">All Categories</button>
+          <button className="sidebar-category">All</button>
           <button className="sidebar-category">Automotive</button>
           <button className="sidebar-category">Arts</button>
           <button className="sidebar-category">Beauty</button>
@@ -163,9 +183,9 @@ function SearchPage() {
           }}
         >
           <h5>Location</h5>
-          <button className="sidebar-category">On-Campus</button>
-          <button className="sidebar-category">ACC Apartments</button>
-          <button className="sidebar-category">UTC Apartments</button>
+          <button className="sidebar-category">Campus</button>
+          <button className="sidebar-category">ACC</button>
+          <button className="sidebar-category">UTC</button>
           <button className="sidebar-category">Other</button>
         </div>
       </div>
