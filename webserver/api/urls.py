22a2bd1dd2789d182d6ed3d50dd8cdd1f00998c4
Alvin Phan <alvintp2@uci.edu>
2023-11-15 01:36:02 -0800

    "Set up URL endpoints for our Rest API Views"

--- webserver/api/urls.py
+++ webserver/api/urls.py
@@ -1,6 +1,11 @@
-from .views import main
 from django.urls import path
+from .views import UserList, ServiceList, ReviewList, BookingList, CategoryList
+
 
 urlpatterns = [
-    path('', main, name='main'),
+    path('users/', UserList.as_view()),
+    path('services/', ServiceList.as_view()),
+    path('reviews/', ReviewList.as_view()),
+    path('bookings/', BookingList.as_view()),
+    path('categories/', CategoryList.as_view()),
 ]
--- webserver/api/views.py
+++ webserver/api/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from rest_framework import generics
+from .models import User, Service, Review, Booking, Category
+from .serializers import UserSerializer, ServiceSerializer, ReviewSerializer, BookingSerializer, CategorySerializer
+
+# Create your API views here.
 
 
-def main(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# Allows us to view all users, services, reviews, bookings, and categories
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ServiceList(generics.ListCreateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class BookingList(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class CategoryList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    
