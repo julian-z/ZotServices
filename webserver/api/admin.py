ad4ce5ef6ee0f2e6591aa065efac6e2a93ef7bf9
Julian Zulfikar <104231911+julian-z@users.noreply.github.com>
2023-11-15 20:08:56 -0800

    Delete webserver directory

--- webserver/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Booking)
-admin.site.register(Category)
-admin.site.register(Review)
-admin.site.register(Service)
-
