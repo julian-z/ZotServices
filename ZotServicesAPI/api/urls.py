f49c80be9e58ba577d5e946ed354c5916a85c3b8
Simon Cao <tcao10@uci.edu>
2023-11-15 20:57:55 -0800

    reset backend

---
+++ ZotServicesAPI/api/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework import routers
+from django.conf.urls import include
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
