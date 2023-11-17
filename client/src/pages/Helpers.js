efcd3b44455667488c0fa12540072066371bfcf7
Julian Zulfikar <jzulfika@uci.edu>
2023-11-17 00:10:27 -0800

    Functional search page

--- client/src/pages/Helpers.js
+++ client/src/pages/Helpers.js
@@ -15,3 +15,19 @@ export async function fetchFromDjango(endpoint) {
 
   return response.json();
 }
+
+export function calculateRating(service) {
+  if (service["ratings"].length === 0) {
+    return 0.0;
+  }
+
+  var totalStars = 0.0;
+  for (let rating of service["ratings"]) {
+    totalStars += parseFloat(rating["rating"]);
+  }
+  return totalStars / service["ratings"].length;
+}
+
+export function formatCategory(category) {
+  return category.charAt(0).toUpperCase() + category.slice(1).toLowerCase();
+}
--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -2,117 +2,105 @@ import React, { useState, useEffect } from "react";
 import "./SearchPage.css";
 import NavBar from "../components/NavBar/NavBar";
 import ServicePreview from "../components/ServicePreview/ServicePreview";
-import { fetchFromDjango } from "./Helpers";
+import { fetchFromDjango, calculateRating, formatCategory } from "./Helpers";
+import { ENDPOINT_URL } from "./Constants";
 
 // Search page - allows browsing through services
-// TODO:
-//  - Implement sorting/filtering
-//  - Connect to API
-//  - Implement infinite scrolling
 function SearchPage() {
-  // TODO: Use SQL data instead of hardcoded examples
-  // const services = [
-  //   {
-  //     sid: 1,
-  //     uid: 1,
-  //     image:
-  //       "https://cdn.discordapp.com/attachments/1174181819793539182/1174411306405736559/image.png?ex=65677ec2&is=655509c2&hm=80aab0c3550427c744e75c27fb7dfa710ddfffd517b3e48760a651a912711fdc&",
-  //     title: "ZotCutz",
-  //     category: "Beauty",
-  //     location: "Stanford Court",
-  //     price: 15.0,
-  //     rating: 5.0,
-  //     description:
-  //       "Line ups, fades, beards, whatever you need. Message me on Instagram (@zotcutz) for available bookings.",
-  //   },
-
-  //   {
-  //     sid: 2,
-  //     uid: 2,
-  //     image:
-  //       "https://cdn.discordapp.com/attachments/1174181819793539182/1174411181625180182/image.png?ex=65677ea5&is=655509a5&hm=40f1f7ba08b47a1b74231bfde9a55987abab3d0114a2be8dc13b31af254d22e0&",
-  //     title: "Custom Petr Stickers",
-  //     category: "Arts",
-  //     location: "Middle Earth",
-  //     price: 2.5,
-  //     rating: 4.8,
-  //     description:
-  //       "Will design a custom Petr sticker of your choice! Base price is $2.50, will increase if commission is hefty.",
-  //   },
-
-  //   {
-  //     sid: 3,
-  //     uid: 3,
-  //     image:
-  //       "https://cdn.discordapp.com/attachments/1174181819793539182/1174461365931487242/image.png?ex=6567ad62&is=65553862&hm=6257ad854e8c8a2521373cd30106305215f070968e838d822ba77693884176f5&",
-  //     title: "AntNails",
-  //     category: "Beauty",
-  //     location: "Mesa Court",
-  //     price: 20.0,
-  //     rating: 4.3,
-  //     description:
-  //       "Get your nails done by Priscilla Anteater at the Caballo tower! Message me for bookings and location.",
-  //   },
-
-  //   {
-  //     sid: 4,
-  //     uid: 4,
-  //     image:
-  //       "https://cdn.discordapp.com/attachments/1174181819793539182/1174410610306461737/image.png?ex=65677e1c&is=6555091c&hm=c488d8cc30ab938741f8cb63f841ec5f584211674c51306b1ab3a43686fea4ff&",
-  //     title: "Floor Cleaning",
-  //     category: "Cleaning",
-  //     location: "Vista Del Campo Norte",
-  //     price: 10.0,
-  //     rating: 4.5,
-  //     description:
-  //       "Professional floor cleaning services provided by VDCN RAs! Must be a VDCN resident to qualify.",
-  //   },
-
-  //   {
-  //     sid: 5,
-  //     uid: 5,
-  //     image:
-  //       "https://cdn.discordapp.com/attachments/1174181819793539182/1174410265211715774/image.png?ex=65677dca&is=655508ca&hm=9cd753e4dcc78c51b83abe1757c3ea6c50457e6cf14ff28ca09fa536c75f197e&",
-  //     title: "Discounted DoorDash",
-  //     category: "Delivery",
-  //     location: "Irvine, CA",
-  //     price: 5.0,
-  //     rating: 4.3,
-  //     description:
-  //       "Send me your DoorDash order and I will deliver it for 75% of the price! Note: $5.00 service fee included.",
-  //   },
-
-  //   {
-  //     sid: 6,
-  //     uid: 6,
-  //     image:
-  //       "https://cdn.discordapp.com/attachments/1174181819793539182/1174410418819694684/image.png?ex=65677def&is=655508ef&hm=df63b290f49666e9cdaf29f1984299d79ce82caf90c8a8de0dc939253d4762ee&",
-  //     title: "Grocery Delivery",
-  //     category: "Delivery",
-  //     location: "Irvine, CA",
-  //     price: 5.0,
-  //     rating: 4.9,
-  //     description:
-  //       "Delivering groceries from any store in Irvine! Most popular: Target, Albertsons, Costco",
-  //   },
-  // ];
-
-  // TODO: Use React hooks to keep track of filters, query, categories, locations
   const [services, setServices] = useState([]);
-  // const [category, setCategory] = useState("all");
+  const [category, setCategory] = useState("all");
+  const [location, setLocation] = useState("all");
+  const [ordering, setOrdering] = useState("none");
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
 
+  // Whenever an attribute is changed
   useEffect(() => {
+    // Format the endpoint that will be called based on the user's selections
+    function formatEndpoint() {
+      if (
+        (category === "all") &
+        (location === "all") &
+        (ordering === "none") &
+        (query === "")
+      ) {
+        console.log(ENDPOINT_URL + "/services/");
+        return "/services/";
+      }
+
+      var queries = [];
+      if (category !== "all") {
+        queries.push(`category=${category}`);
+      }
+      if (location !== "all") {
+        queries.push(`location=${location}`);
+      }
+      if (ordering !== "none") {
+        queries.push(`ordering=${ordering}`);
+      }
+      if (query !== "") {
+        queries.push(`search=${query}`);
+      }
+      console.log(ENDPOINT_URL + "/services?" + queries.join("&")); // DEBUG
+      return "/services?" + queries.join("&");
+    }
+
+    // Asynchronously fetch the data after formatting endpoint
     const fetchData = async (endpoint) => {
       try {
         const data = await fetchFromDjango(endpoint);
         setServices(data);
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error("ERROR FETCHING DATA:", error);
       }
     };
+    fetchData(formatEndpoint());
 
-    fetchData("/services/");
-  }, []);
+    // Remove loading icon
+    setLoading(false);
+  }, [category, location, ordering, query]);
+
+  // Once a category is chosen
+  function fetchCategory(c) {
+    if (c === category) {
+      return;
+    }
+    setLoading(true);
+    setServices([]);
+    setCategory(c);
+  }
+
+  // Once a location is chosen
+  function fetchLocation(l) {
+    if (l === location) {
+      return;
+    }
+    setLoading(true);
+    setServices([]);
+    setLocation(l);
+  }
+
+  // Once an ordering is chosen
+  function fetchOrdering(o) {
+    console.log(o);
+    if (o === ordering) {
+      return;
+    }
+    setLoading(true);
+    setServices([]);
+    setOrdering(o);
+  }
+
+  // Once a query is submitted
+  const fetchQuery = () => {
+    const q = document.getElementById("sidebar-search").value;
+    if (q === query) {
+      return;
+    }
+    setLoading(true);
+    setServices([]);
+    setQuery(encodeURIComponent(q));
+  };
 
   return (
     <div className="row">
@@ -128,8 +116,6 @@ function SearchPage() {
           height: "100vh",
         }}
       >
-        <h2>All Services</h2>
-
         <div
           style={{
             display: "flex",
@@ -147,31 +133,36 @@ function SearchPage() {
                 uid={service["user"]}
                 image={service["image"]}
                 title={service["title"]}
-                category={service["category"]}
-                location={service["location"]}
+                category={formatCategory(service["category"])}
+                location={service["spec_location"]}
                 price={parseFloat(service["pricing"])}
-                // rating={service["rating"]} // TODO: dynamic ratings
-                rating={0}
+                rating={calculateRating(service)}
               ></ServicePreview>
             );
           })}
 
-          {/* TODO: Infinite scrolling */}
-          {/* <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              gap: "1vw",
-              width: "100%",
-            }}
+          {/* Loading animation */}
+          <div
+            style={
+              loading
+                ? {
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "1vw",
+                    width: "100%",
+                  }
+                : {
+                    display: "none",
+                  }
+            }
           >
-            <p style={{ margin: "0", padding: "10vh 0", fontWeight: "500" }}>
-              Loading more services...
+            <p style={{ margin: "0", padding: "5vh 0", fontWeight: "500" }}>
+              Loading Services...
             </p>
             <div className="loading-spinner"></div>
-          </div> */}
+          </div>
         </div>
       </div>
 
@@ -192,12 +183,33 @@ function SearchPage() {
         <h4>Browse 🔎</h4>
 
         {/* Search bar */}
-        <form>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "stretch",
+            gap: "0.5vw",
+          }}
+          onSubmit={(e) => {
+            e.preventDefault();
+            fetchQuery();
+          }}
+        >
           <input
             placeholder="Search by title..."
             autoComplete="off"
             id="sidebar-search"
-          ></input>
+          />
+          <button
+            type="submit"
+            className="sidebar-category"
+            style={{ margin: "0" }}
+            id="search-button"
+            disabled={loading}
+          >
+            Search
+          </button>
         </form>
 
         <hr></hr>
@@ -211,8 +223,17 @@ function SearchPage() {
           }}
         >
           <h5>Sort By</h5>
-          <button className="sidebar-category">Price</button>
-          <button className="sidebar-category">Ratings</button>
+          <select
+            className="sidebar-category"
+            style={{ width: "100%" }}
+            onChange={(e) => fetchOrdering(e.target.value)}
+          >
+            <option value="none">None</option>
+            <option value="+pricing">Price (Low to High)</option>
+            <option value="-pricing">Price (High to Low)</option>
+            <option value="+average_ratings">Ratings (Low to High)</option>
+            <option value="-average_ratings">Ratings (High to Low)</option>
+          </select>
         </div>
 
         <hr></hr>
@@ -226,15 +247,85 @@ function SearchPage() {
           }}
         >
           <h5>Categories</h5>
-          <button className="sidebar-category">All</button>
-          <button className="sidebar-category">Automotive</button>
-          <button className="sidebar-category">Arts</button>
-          <button className="sidebar-category">Beauty</button>
-          <button className="sidebar-category">Cleaning</button>
-          <button className="sidebar-category">Clothing</button>
-          <button className="sidebar-category">Deliveries</button>
-          <button className="sidebar-category">Miscellaneous</button>
-          <button className="sidebar-category">Pets</button>
+          <button
+            className={`sidebar-category ${category === "all" ? "active" : ""}`}
+            id="all"
+            onClick={() => fetchCategory("all")}
+          >
+            All
+          </button>
+          <button
+            className={`sidebar-category ${
+              category === "automotive" ? "active" : ""
+            }`}
+            id="automotive"
+            onClick={() => fetchCategory("automotive")}
+          >
+            Automotive
+          </button>
+          <button
+            className={`sidebar-category ${
+              category === "arts" ? "active" : ""
+            }`}
+            id="arts"
+            onClick={() => fetchCategory("arts")}
+          >
+            Arts
+          </button>
+          <button
+            className={`sidebar-category ${
+              category === "beauty" ? "active" : ""
+            }`}
+            id="beauty"
+            onClick={() => fetchCategory("beauty")}
+          >
+            Beauty
+          </button>
+          <button
+            className={`sidebar-category ${
+              category === "cleaning" ? "active" : ""
+            }`}
+            id="cleaning"
+            onClick={() => fetchCategory("cleaning")}
+          >
+            Cleaning
+          </button>
+          <button
+            className={`sidebar-category ${
+              category === "clothing" ? "active" : ""
+            }`}
+            id="clothing"
+            onClick={() => fetchCategory("clothing")}
+          >
+            Clothing
+          </button>
+          <button
+            className={`sidebar-category ${
+              category === "deliveries" ? "active" : ""
+            }`}
+            id="deliveries"
+            onClick={() => fetchCategory("deliveries")}
+          >
+            Deliveries
+          </button>
+          <button
+            className={`sidebar-category ${
+              category === "miscellaneous" ? "active" : ""
+            }`}
+            id="miscellaneous"
+            onClick={() => fetchCategory("miscellaneous")}
+          >
+            Miscellaneous
+          </button>
+          <button
+            className={`sidebar-category ${
+              category === "pets" ? "active" : ""
+            }`}
+            id="pets"
+            onClick={() => fetchCategory("pets")}
+          >
+            Pets
+          </button>
         </div>
 
         <hr></hr>
@@ -248,10 +339,45 @@ function SearchPage() {
           }}
         >
           <h5>Location</h5>
-          <button className="sidebar-category">Campus</button>
-          <button className="sidebar-category">ACC</button>
-          <button className="sidebar-category">UTC</button>
-          <button className="sidebar-category">Other</button>
+          <button
+            className={`sidebar-category ${location === "all" ? "active" : ""}`}
+            id="all"
+            onClick={() => fetchLocation("all")}
+          >
+            All
+          </button>
+          <button
+            className={`sidebar-category ${
+              location === "campus" ? "active" : ""
+            }`}
+            id="campus"
+            onClick={() => fetchLocation("campus")}
+          >
+            Campus
+          </button>
+          <button
+            className={`sidebar-category ${location === "acc" ? "active" : ""}`}
+            id="acc"
+            onClick={() => fetchLocation("acc")}
+          >
+            ACC
+          </button>
+          <button
+            className={`sidebar-category ${location === "utc" ? "active" : ""}`}
+            id="utc"
+            onClick={() => fetchLocation("utc")}
+          >
+            UTC
+          </button>
+          <button
+            className={`sidebar-category ${
+              location === "other" ? "active" : ""
+            }`}
+            id="other"
+            onClick={() => fetchLocation("other")}
+          >
+            Other
+          </button>
         </div>
       </div>
     </div>
